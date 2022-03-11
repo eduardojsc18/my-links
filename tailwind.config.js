bd32636e7c1933d980c963c201643c9e1ce46c2f
Eduardo José <eduardojsc18@outlook.com>
2022-03-10 23:39:45 -0300

    🖼️ Iniciado construção do layout do Header
    [-- ## commit de backup ## --]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    fontFamily: {
+      'Raleway': ['"Raleway"', 'sans-serif'],
+    },
+
+    extend: {
+
+    },
+
   },
   plugins: [],
 }
